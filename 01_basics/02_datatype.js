ada492df246833dd172f9fb81c714aab1b12109b
Abdullah2139 <abdullahjan2780@gmail.com>
2025-01-01 17:06:55 +0500

    Add code

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,15 @@
+let name="John"
+let age=25
+let isMarried=false
+// null(standalone value) is a special value that represents nothing, empty or unknown
+let car=null
+// undefined is a special value that represents the absence of a value
+let job
+// symbol is a unique and immutable data type that is often used to identify object properties
+let id=Symbol('id')
+// bigint is a special numeric type that provides support for integers of arbitrary length
+let bigInt=1234567890123456789012345678901234567890n
+// console.log(name, age, isMarried, car, job, id, bigInt)
+// console.log(typeof name, typeof age, typeof isMarried, typeof car, typeof job, typeof id, typeof bigInt)
+// typeof operator returns the data type of a variable or an expression
+console.log(typeof name)
